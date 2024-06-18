f47300637ae613b8453c7009be223f8792f1c5ff
Huseyin Karatas <karatashuseyin2016@gmail.com>
2024-06-18 15:58:05 +0300

    Sitemap Update

--- app/sitemap.js
+++ app/sitemap.js
@@ -4,13 +4,25 @@ export default async function sitemap() {
   const blogs = await getBlogs();
   const blogEntries = blogs.map((blog) => ({
     url: `https://performanslab.com/${blog.slug}`,
-    // lastModified: new Date(blog._updatedAt)
-    // changeFreq: "",
-    // priority: 0.8
+    lastModified: new Date(blog._updatedAt).toISOString().split("T")[0],
+    priority: 0.9,
+    changefreq: "weekly",
   }));
   return [
     {
       url: "https://performanslab.com/",
+      priority: 1,
+      changefreq: "weekly",
+    },
+    {
+      url: "https://performanslab.com/iletisim",
+      priority: 0.6,
+      changefreq: "weekly",
+    },
+    {
+      url: "https://performanslab.com/arsiv",
+      priority: 0.8,
+      changefreq: "weekly",
     },
     ...blogEntries,
   ];
