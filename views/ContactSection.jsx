e68a9ce528c7b4be2a710ad9804467206cc06955
karatashsyn <81769362+karatashsyn@users.noreply.github.com>
2026-05-06 19:40:45 +0300

    remove map

--- views/ContactSection.jsx
+++ views/ContactSection.jsx
@@ -32,12 +32,12 @@ export default function ContactSection({
           src="/fake-img.png"
           alt="fatih-ozkan"
         >
-          <iframe
+          {/* <iframe
             title="PerformansLab Levent Haritası"
             className="w-full rounded-md aspect-[2]"
             src="https://maps.google.com/maps?q=34394%20%C5%9Ei%C5%9Fli%2F%C4%B0stanbul%20performanslab%20Levent&t=m&z=15&output=embed&iwloc=near"
             frameBorder="0"
-          ></iframe>
+          ></iframe> */}
         </div>
         <ul className="pt-4 flex flex-col max-sm:items-start max-sm:w-full gap-10 basis-full">
           <ContactInfo
@@ -54,20 +54,20 @@ export default function ContactSection({
             label={"Mail"}
             icon="/message.svg"
           />
-          <ContactInfo
+          {/* <ContactInfo
             textClass={lightMode ? "!text-[#09090b] arial" : ""}
             labelClass={lightMode ? "!text-[#555] arial" : ""}
             info={"Atom Sk. No:22-32, 34394 Şişli/İstanbul"}
             label={"Adres"}
             icon="/pin.svg"
-          />
+          /> */}
         </ul>
         <div className="basis-full max-sm:hidden rounded-md overflow-hidden bg-gray-500 max-sm:w-[90%]">
-          <iframe
+          {/* <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d255.0098647343109!2d29.008880689933875!3d41.07717282188019!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14cab75094b5195f%3A0x562bfeef9f4801a7!2sPerformanslab%20Levent!5e0!3m2!1str!2str!4v1750218629408!5m2!1str!2str"
             className="w-full rounded-md sm:aspect-[666/307]"
             title="PerformansLab Levent Haritası"
-          ></iframe>
+          ></iframe> */}
         </div>
       </div>
     </section>
